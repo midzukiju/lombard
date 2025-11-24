f5b5b460125a8e5ec9d9369a7ed57045cd08cd0d
Васина Алёна Михайловна <10230901@college.local>
2025-11-24 10:30:47 +0300

    lombard2

--- lombard/MainWindow.xaml.cs
+++ lombard/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace lombard
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
